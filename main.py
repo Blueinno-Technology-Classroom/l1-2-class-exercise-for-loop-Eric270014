48c5c1b4370a6e95b57ebad3f763a70368f8b4e4
Eric <ericzjchee@gmail.com>
2024-01-13 14:53:45 +0800

    Hi, For loop lesson

--- main.py
+++ main.py
@@ -1,43 +1,94 @@
 ##################################################
 '''
-Q1: 
+Q1:
+for cycle in range(0, 100001, 1):
+    print(cycle)
 '''
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q2:
+result = 0 
+for cycle_number in range(0, 100001, 1):
+    result = result + cycle_number
+print(f'The sum is {result}')
 '''
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q3:
+for one in range(2, 30, 2):
+    print(one)
 '''
+
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q4:
+for two in range(97, 0, -2):
+    print(two)
+
 '''
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q5:
+for i in range(1, 11, 1):
+    print(i*i)
+
+for i in range(1, 11, 1):
+    print(i**2)
+
 '''
+
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q6:
+for i in range(5):
+    print('*' * 5)
+
 '''
+
 
 # TODO: Write your code here
 
 ##################################################
+
+'''
+Q7
+for i in range(1, 6, 1):
+    print(i*'*')
+'''
+
+
+'''
+Q8
+for i in range(6, 0, -1):
+    print(i*'*')
+'''
+
+'''
+Q9
+
+for i in range(1, 6, 1):
+    print(f'{i}' * i)
+
+'''
+
+
